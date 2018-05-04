8a20041977a7dbf8e49ba5f7862dc6aed963b907
PatskoArkadzi <arkpatsko@gmail.com>
2018-05-04 20:08:09 +0300

    trigger for generating orderNumber, fill in the `seats` table

--- sql/initDBCinema.sql
+++ sql/initDBCinema.sql
@@ -22,40 +22,44 @@ DROP TABLE IF EXISTS `films`;
 CREATE TABLE IF NOT EXISTS `films` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `filmName` varchar(50) NOT NULL,
-  `genre` varchar(50) NOT NULL,
-  `description` varchar(500) NOT NULL,
+  `description` varchar(2000) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 -- Экспортируемые данные не выделены.
--- Дамп структуры для таблица cinema.filmsessions
-DROP TABLE IF EXISTS `filmsessions`;
-CREATE TABLE IF NOT EXISTS `filmsessions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+-- Дамп структуры для таблица cinema.films_genres
+DROP TABLE IF EXISTS `films_genres`;
+CREATE TABLE IF NOT EXISTS `films_genres` (
   `film_id` int(11) NOT NULL,
-  `date` varchar(50) NOT NULL,
-  `time` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`),
+  `genre_id` int(11) NOT NULL,
   KEY `film_id` (`film_id`),
-  CONSTRAINT `FK_sessions_films` FOREIGN KEY (`film_id`) REFERENCES `films` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='киносеансы';
+  KEY `genre_id` (`genre_id`),
+  CONSTRAINT `FK_films_genres_films` FOREIGN KEY (`film_id`) REFERENCES `films` (`id`),
+  CONSTRAINT `FK_films_genres_genres` FOREIGN KEY (`genre_id`) REFERENCES `genres` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Экспортируемые данные не выделены.
+-- Дамп структуры для таблица cinema.genres
+DROP TABLE IF EXISTS `genres`;
+CREATE TABLE IF NOT EXISTS `genres` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `genreName` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Экспортируемые данные не выделены.
 -- Дамп структуры для таблица cinema.orders
 DROP TABLE IF EXISTS `orders`;
 CREATE TABLE IF NOT EXISTS `orders` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `orderNumber` int(10) NOT NULL COMMENT 'generated column',
+  `orderNumber` int(10) NOT NULL COMMENT 'trigger-generated column',
   `user_id` int(11) NOT NULL,
-  `ticket_id` int(10) NOT NULL,
   `isPaid` bit(1) NOT NULL DEFAULT b'0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `orderNumber` (`orderNumber`),
-  KEY `ticket_id` (`ticket_id`),
   KEY `user_id` (`user_id`),
-  CONSTRAINT `FK_orders_tickets` FOREIGN KEY (`ticket_id`) REFERENCES `tickets` (`id`),
   CONSTRAINT `FK_orders_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- Экспортируемые данные не выделены.
 -- Дамп структуры для таблица cinema.roles
@@ -74,7 +78,20 @@ CREATE TABLE IF NOT EXISTS `seats` (
   `row` int(11) NOT NULL,
   `number` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=251 DEFAULT CHARSET=utf8;
+
+-- Экспортируемые данные не выделены.
+-- Дамп структуры для таблица cinema.sessions
+DROP TABLE IF EXISTS `sessions`;
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `film_id` int(11) NOT NULL,
+  `date` varchar(50) NOT NULL,
+  `time` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `film_id` (`film_id`),
+  CONSTRAINT `FK_film_sessions_films` FOREIGN KEY (`film_id`) REFERENCES `films` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='киносеансы';
 
 -- Экспортируемые данные не выделены.
 -- Дамп структуры для таблица cinema.tickets
@@ -83,12 +100,15 @@ CREATE TABLE IF NOT EXISTS `tickets` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `session_id` int(11) NOT NULL,
   `seat_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
   `price` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `seat_id` (`seat_id`),
   KEY `session_id` (`session_id`),
-  CONSTRAINT `FK_tickets_sessions` FOREIGN KEY (`session_id`) REFERENCES `filmsessions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_tikets_seats` FOREIGN KEY (`seat_id`) REFERENCES `seats` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `order_id` (`order_id`),
+  CONSTRAINT `FK_tickets_film_sessions` FOREIGN KEY (`session_id`) REFERENCES `sessions` (`id`),
+  CONSTRAINT `FK_tickets_orders` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+  CONSTRAINT `FK_tickets_seats` FOREIGN KEY (`seat_id`) REFERENCES `seats` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Экспортируемые данные не выделены.
@@ -106,6 +126,16 @@ CREATE TABLE IF NOT EXISTS `users` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- Экспортируемые данные не выделены.
+-- Дамп структуры для триггер cinema.generate orderNumber
+DROP TRIGGER IF EXISTS `generate orderNumber`;
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
+DELIMITER //
+CREATE TRIGGER `generate orderNumber` BEFORE INSERT ON `orders` FOR EACH ROW BEGIN
+	SET NEW.orderNumber = (RAND()*(999999-100000)+100000);
+END//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
+
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
